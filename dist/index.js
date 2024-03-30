f84582ae09d2231e74124c996a25477c9272b043
vivek-panchal <vivek.nsut@gmail.com>
2024-03-30 14:57:39 +0530

    Array & Objects

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ let check;
 let m; //any
 let Job = "Remote";
 let surname; //Union type
-//Array 
+//**************Array****************
 //Type 1 of declaration
 const arr = [12, 5, 47, 89, 54];
 const arr2 = ["Vivek", "Panchal"];
@@ -18,3 +18,40 @@ const arr5 = [26, "Aug"];
 arr2[0] = "Mr";
 //Tupple or Fix sized Array
 const arr6 = [26, "Aug", true];
+const obj = {
+    height: 3434,
+    weight: 78,
+    gender: "Male"
+};
+const obj2 = {
+    height: 334,
+    weight: 48,
+    gender: "Femal"
+};
+const obj3 = {
+    height: 434,
+    weight: 59,
+};
+//*****************Function***************
+const func = (n, m) => {
+    return n * m;
+};
+//Optional parameter
+const multi = (a, b, c) => {
+    if (typeof c === "undefined")
+        return a * b;
+    return a * b * c;
+};
+const result1 = multi(25, 58);
+const result2 = multi(65, 58, 78);
+//Default parameter
+const multi2 = (a, b, c = 25) => {
+    return a * b * c;
+};
+const result3 = multi2(25, 47);
+const result4 = multi2(65, 29, 38);
+//Rest Operator
+const funcarr = (...m) => {
+    return m;
+};
+funcarr(25, 58, 87, 96, 71, 62);
--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ let Job=<string>"Remote";
 
 let surname:string | number; //Union type
 
-//Array 
+//**************Array****************
 
 //Type 1 of declaration
 const arr: number[]=[12,5,47,89,54];
@@ -27,3 +27,64 @@ arr2[0]="Mr";
 //Tupple or Fix sized Array
 
 const arr6:[number,string,boolean] = [26,"Aug",true];
+
+
+//************Objects in typescript*********
+
+type Obj ={
+    height: number;
+    weight: number;
+    gender?: string; //Optional
+}
+
+const obj:Obj={
+    height:3434,
+    weight: 78,
+    gender:"Male"
+}
+
+const obj2:Obj={
+    height:334,
+    weight: 48,
+    gender:"Femal"
+}
+
+const obj3:Obj={
+    height:434,
+    weight: 59,
+}
+
+
+//*****************Function***************
+
+const func = (n:number , m:number) => {
+    return n*m;
+}
+
+type FuncType = (x:number , y:number, z?: number) => number;
+
+//Optional parameter
+const multi:FuncType =(a,b,c) =>{
+   if(typeof c === "undefined") return a*b;
+    return a*b*c;
+}
+
+const result1=multi(25,58);
+
+const result2=multi(65,58,78);
+
+//Default parameter
+const multi2:FuncType =(a,b,c=25) =>{
+    return a*b*c;
+}
+
+const result3=multi2(25,47);
+
+const result4=multi2(65,29,38);
+
+//Rest Operator
+const funcarr =(...m:number[]) => {
+    return m;
+}
+
+funcarr(25, 58, 87, 96, 71, 62)
